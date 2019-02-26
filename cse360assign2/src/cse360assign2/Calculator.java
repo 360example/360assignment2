71a7814d98e1087405d8564d6f9b776818e39a74
John <John@My-WeirdButOK-Flex>
2019-02-25 22:49:55 -0700

    Added string functionality, added comments, and added git info

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -5,6 +5,13 @@
  * 
  * Description:	This file contains the class Calculator,
  * 		described below
+ * 
+ * github repository:
+ * 		https://github.com/360example/360assignment2.git
+ * username:
+ * 		360example
+ * password:
+ * 		360password3
  */
 package cse360assign2;
 
@@ -12,10 +19,12 @@ package cse360assign2;
  * Calculator class
  * 		The purpose of this class is to keep a running total
  * 		of a number as various operations are performed
+ * 		while also maintaining a string of calculation history
  */
 public class Calculator {
 
 	private int total;
+	private String historyString = "0";
 	
 	/*
 	 * Constructor, initializes total for clarity
@@ -28,61 +37,65 @@ public class Calculator {
 	 * getTotal
 	 * @params: none
 	 * @return: 0...?
-	 * @description, probably a getter, should return total
+	 * @description, returns private member "total"
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/*
 	 * add
 	 * @params: value
 	 * @return: void
-	 * @description: adds total and value, probably adds addition to string
+	 * @description: adds total and value, adds addition to string
 	 */
 	public void add (int value) {
-		total += value;		
+		total += value;
+		historyString += " + " + value;
 	}
 	
 	/*
 	 * subtract
 	 * @params: value
 	 * @return: void
-	 * @description: subtracts value from total, probably adds subtraction to string
+	 * @description: subtracts value from total, adds subtraction to string
 	 */
 	public void subtract (int value) {
 		total -= value;
+		historyString += " - " + value;
 	}
 	
 	/*
 	 * multiply
 	 * @params: value
 	 * @return: void
-	 * @description: multiplies total by value, probably adds multiplication to string
+	 * @description: multiplies total by value, adds multiplication to string
 	 */
 	public void multiply (int value) {
 		total *= value;
+		historyString += " * " + value;
 	}
 	
 	/*
 	 * divide
 	 * @params: void
 	 * @return: void
-	 * @description: divides total by value, probably adds division to string
+	 * @description: divides total by value, adds division to string
 	 */
 	public void divide (int value) {
 		if (value != 0)
 			total = total / value;
 		else
 			total = 0;
+		historyString += " / " + value;
 	}
 	
 	/*
 	 * getHistory
 	 * @params: none
-	 * @return: string of history
+	 * @return: string of history, generated via each calculation
 	 */
 	public String getHistory () {
-		return "";
+		return historyString;
 	}
 }
--- cse360assign2/src/cse360assign2/cse360assign2.java
+++ cse360assign2/src/cse360assign2/cse360assign2.java
@@ -4,8 +4,26 @@ package cse360assign2;
 public class cse360assign2 {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World");
+		System.out.println("Hello World");
 		Calculator myCalc = new Calculator();
+		
+		myCalc.add(1);
+		System.out.println(myCalc.getHistory());
+		myCalc.add(10);
+		System.out.print(myCalc.getHistory());
+		System.out.println(" = " + myCalc.getTotal());
+		myCalc.subtract(1);
+		System.out.print(myCalc.getHistory());
+		System.out.println(" = " + myCalc.getTotal());
+		myCalc.multiply(1);
+		System.out.print(myCalc.getHistory());
+		System.out.println(" = " + myCalc.getTotal());
+		myCalc.divide(5);
+		System.out.print(myCalc.getHistory());
+		System.out.println(" = " + myCalc.getTotal());
+		myCalc.add(1);
+		System.out.print(myCalc.getHistory());
+		System.out.println(" = " + myCalc.getTotal());
 
 	}
 
